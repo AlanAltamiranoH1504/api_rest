a6fb7f0774dc9254b3e283d56cbd8093f8b0431e
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-01-29 17:00:34 -0600

    Inicio del proyecto

---
+++ src/main/java/altamirano/hernandez/api_restfiull/ApiRestFiullApplication.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.api_restfiull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestFiullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestFiullApplication.class, args);
+    }
+
+}
